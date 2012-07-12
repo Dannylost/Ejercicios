71f6ba71db06595331516dc2798dcc7da9d6a448
Dannylost <dannylost_13@hotmail.com>
2012-07-12 05:18:12 -0500

    batalla naval

---
+++ MTA2/src/juegoBatallaNaval/SimpleDotCom.java
@@ -0,0 +1,28 @@
+package juegoBatallaNaval;
+
+public class SimpleDotCom {
+	
+	int [] locationCells;
+	int numOfHits;
+	
+		
+	public SimpleDotCom(int[] locationCells) {
+		super();
+		this.locationCells = locationCells;
+	}
+
+
+	public void checkYourself (String numGuess){
+		
+		
+	}
+
+
+	public void setLocationCells(int[] locationCells) {
+		this.locationCells = locationCells;
+	}
+	
+	
+	
+	
+}
